7aa83e6e9c9afd180cb810e128ff3507fddd4ee2
ImmortalSdm <1245046380@qq.com>
2023-03-30 08:17:38 +0000

    edit loss

--- main_pretrain.py
+++ main_pretrain.py
@@ -247,7 +247,6 @@ def main(args):
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
             data_loader_train.sampler.set_epoch(epoch)
-        my_validate(model, data_loader_val, device, epoch, log_writer=log_writer, args=args)
         train_stats = my_train_one_epoch(
             model, data_loader_train,
             optimizer, device, epoch, loss_scaler,
